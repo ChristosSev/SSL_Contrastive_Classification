d94b112d558ea1536be6f5f74ad7441771d1525e
Christos Sevastopoulos <45820335+ChristosSev@users.noreply.github.com>
2022-09-19 15:22:12 -0500

    Create NNCLR.py

---
+++ NNCLR.py
@@ -0,0 +1,222 @@
+# Note: The model and training settings do not follow the reference settings
+# from the paper. The settings are chosen such that the example can easily be
+# run on a small dataset with a single GPU.
+
+import torch
+from torch import nn
+import torchvision
+import pytorch_lightning as pl
+import lightly
+from lightly.data import LightlyDataset
+from lightly.data import SimCLRCollateFunction
+from lightly.loss import NTXentLoss
+from lightly.models.modules import NNCLRProjectionHead
+from lightly.models.modules import NNCLRPredictionHead
+from lightly.models.modules import NNMemoryBankModule
+from lightly.models.utils import deactivate_requires_grad
+from lightly.models.utils import update_momentum
+from lightly.models.utils import batch_shuffle
+from lightly.models.utils import batch_unshuffle
+
+max_epochs = 100
+
+img_path = '/home/christos_sevastopoulos/Desktop/Extracted_Data/uc2/img/input_folder'
+path_to_train = '/home/christos_sevastopoulos/Desktop/TOY_DATASET/TRAINING'
+path_to_test = '/home/christos_sevastopoulos/Desktop/TOY_DATASET/TESTING'
+
+class NNCLR(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+
+        self.backbone = backbone
+        self.projection_head = NNCLRProjectionHead(512, 512, 128)
+        self.prediction_head = NNCLRPredictionHead(128, 512, 128)
+
+    def forward(self, x):
+        y = self.backbone(x).flatten(start_dim=1)
+        z = self.projection_head(y)
+        p = self.prediction_head(z)
+        z = z.detach()
+        return z, p
+
+
+resnet = torchvision.models.resnet18()
+backbone = nn.Sequential(*list(resnet.children())[:-1])
+model = NNCLR(backbone)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+memory_bank = NNMemoryBankModule(size=4096)
+memory_bank.to(device)
+
+class Classifier(pl.LightningModule):
+    def __init__(self, backbone):
+        super().__init__()
+        # use the pretrained ResNet backbone
+        self.backbone = backbone
+
+        # freeze the backbone
+        deactivate_requires_grad(backbone)
+
+        # create a linear layer for our downstream classification model
+        self.fc = nn.Linear(512, 10)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        y_hat = self.backbone(x).flatten(start_dim=1)
+        y_hat = self.fc(y_hat)
+        return y_hat
+
+    def training_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_hat = self.forward(x)
+        loss = self.criterion(y_hat, y)
+        self.log("train_loss_fc", loss)
+        return loss
+
+    def training_epoch_end(self, outputs):
+        self.custom_histogram_weights()
+
+    # We provide a helper method to log weights in tensorboard
+    # which is useful for debugging.
+    def custom_histogram_weights(self):
+        for name, params in self.named_parameters():
+            self.logger.experiment.add_histogram(
+                name, params, self.current_epoch
+            )
+
+    def validation_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_hat = self.forward(x)
+        y_hat = torch.nn.functional.softmax(y_hat, dim=1)
+
+        # calculate number of correct predictions
+        _, predicted = torch.max(y_hat, 1)
+        num = predicted.shape[0]
+        correct = (predicted == y).float().sum()
+        return num, correct
+
+    def validation_epoch_end(self, outputs):
+        # calculate and log top1 accuracy
+        if outputs:
+            total_num = 0
+            total_correct = 0
+            for num, correct in outputs:
+                total_num += num
+                total_correct += correct
+            acc = total_correct / total_num
+            self.log("val_acc", acc, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        optim = torch.optim.SGD(self.fc.parameters(), lr=0.001)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
+        return [optim], [scheduler]
+
+dataset = LightlyDataset(img_path)
+
+batch_size = 256
+num_workers=8
+collate_fn = SimCLRCollateFunction(input_size=32)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+criterion = NTXentLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+
+
+
+train_classifier_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.RandomCrop(32, padding=4),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+# No additional augmentations for the test set
+test_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((32, 32)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+
+dataset_train_classifier = lightly.data.LightlyDataset(
+    input_dir=path_to_train,
+    transform=train_classifier_transforms
+)
+
+dataset_test = lightly.data.LightlyDataset(
+    input_dir=path_to_test,
+    transform=test_transforms
+)
+
+
+dataloader_train_classifier = torch.utils.data.DataLoader(
+    dataset_train_classifier,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers
+)
+
+dataloader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers
+)
+
+
+print("Starting Training")
+for epoch in range(100):
+    total_loss = 0
+    for (x0, x1), _, _ in dataloader:
+
+        x0 = x0.to(device)
+        x1 = x1.to(device)
+        z0, p0 = model(x0)
+        z1, p1 = model(x1)
+        z0 = memory_bank(z0, update=False)
+        z1 = memory_bank(z1, update=True)
+        z0 = z0.to(device)
+        z1 = z1.to(device)
+        loss = 0.5 * (criterion(z0, p1) + criterion(z1, p0))
+        total_loss += loss.detach()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    avg_loss = total_loss / len(dataloader)
+    print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
+
+    gpus = 1 if torch.cuda.is_available() else 0
+
+model.cuda()
+model.eval()
+classifier = Classifier(model.backbone)
+trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus,
+                         progress_bar_refresh_rate=100)
+trainer.fit(
+        classifier,
+        dataloader_train_classifier,
+        dataloader_test
+    )
+
+
+
